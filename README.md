c87c77d625ad003f02429d9837ebac2f0511eeee
ahmed <ahmed.elghoul@infor.com>
2020-11-29 12:32:03 +0100

    add store in product module

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
  ng add @ngrx/schematics
  ng add @ngrx/store
+
+ng g m product --route "product" -m app.module.ts
+ng g store --statePath=product/store -m product/product.module.ts --stateInterface=productState
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
+import { StoreModule } from '@ngrx/store';
+import * as fromProduct from './store';
 
 
 @NgModule({
   declarations: [ProductComponent],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    StoreModule.forFeature(fromProduct.productFeatureKey, fromProduct.reducers, { metaReducers: fromProduct.metaReducers })
   ]
 })
 export class ProductModule { }
