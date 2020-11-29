b564acf6950fa3295b2399f8870dc95a888d5a9f
ahmed <ahmed.elghoul@infor.com>
2020-11-29 12:41:18 +0100

    add product  services and components

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './services/product.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,14 +6,18 @@ import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
 import { StoreModule } from '@ngrx/store';
 import * as fromProduct from './store';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 
 
 @NgModule({
-  declarations: [ProductComponent],
+  declarations: [ProductComponent, ProductListComponent, ProductEditComponent, ProductAddComponent],
   imports: [
     CommonModule,
     ProductRoutingModule,
     StoreModule.forFeature(fromProduct.productFeatureKey, fromProduct.reducers, { metaReducers: fromProduct.metaReducers })
-  ]
+  ],
+  providers:[ProductService]
 })
 export class ProductModule { }
