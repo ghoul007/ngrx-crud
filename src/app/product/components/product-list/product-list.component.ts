58a384e1d060b47fd5e703c79c8d672f66a09dc5
ahmed <ahmed.elghoul@infor.com>
2020-11-29 22:26:41 +0100

    add action

--- src/app/product/components/product-list/product-list.component.ts
+++ src/app/product/components/product-list/product-list.component.ts
@@ -1,7 +1,10 @@
+import { ProductState } from './../../store/index';
 import { Component, OnInit } from "@angular/core";
 import { Product } from "../../models/product";
 import { ProductService } from "../../services/product.service";
 import { Router } from "@angular/router";
+import { Store } from '@ngrx/store';
+import *  as fromActions from "../../store/product.actions";
 
 @Component({
   selector: "app-product-list",
@@ -11,16 +14,26 @@ import { Router } from "@angular/router";
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService, public router: Router) {}
+  constructor(private productService: ProductService,
+    public router: Router,
+    private store: Store<ProductState>
+  ) { }
 
   ngOnInit() {
+    this.store.dispatch(fromActions.loadProducts());
     this.loadProducts();
   }
 
   loadProducts() {
     const productsObserver = {
-      next: products => (this.products = products),
-      error: err => console.error(err)
+      next: products => {
+        this.store.dispatch(fromActions.loadProductsSuccess({ products }));
+        this.products = products
+      },
+      error: error => {
+        this.store.dispatch(fromActions.loadProductsFailure({ error }));
+        console.error(error)
+      }
     };
 
     this.productService.getProducts().subscribe(productsObserver);
